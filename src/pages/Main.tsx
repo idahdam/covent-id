c5fb38bf414e7d5b9f374de69a7af30d4c2231b1
Hadi <nasshitell@gmail.com>
2021-07-28 14:23:37 +0700

    map

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Main = () => {
-  return <div>COVENT MAIN PAGE</div>;
+  return (
+    <>
+      <iframe
+        title="Persebaran Product Covent Indonesia"
+        src="https://www.google.com/maps/d/u/3/embed?mid=1MRMe4JY-u82g-_4B6SQkxOi_hBVVSLco"
+        width="640"
+        height="480"
+      ></iframe>
+    </>
+  );
 };
 
 export default Main;

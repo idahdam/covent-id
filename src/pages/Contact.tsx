4793a416493d78d0020ae10a3db77f4b3d897c3a
Hadi <nasshitell@gmail.com>
2021-07-31 12:53:05 +0700

    contect page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -7,7 +7,72 @@ const Contact = () => {
       <div className="container">
         <div className="banner">CONTACT US!</div>
       </div>
-      <div>Contact</div>
+      <div className="contact-us-container">
+        <div className="contact-us-title">Contact form</div>
+        <div className="contact-us-row">
+          <div className="contact-us-column">
+            <div className="contact-us-input-each">
+              <div className="contact-us-input-label">
+                <label>First name*</label>
+              </div>
+              <div>
+                <input type="text" className="contact-us-input-area" required />
+              </div>
+              <div className="contact-us-input-label">
+                <label>Email*</label>
+              </div>
+              <div>
+                <input
+                  type="email"
+                  className="contact-us-input-area"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className="contact-us-column">
+            <div className="contact-us-input-each">
+              <div className="contact-us-input-label">
+                <label>Last name*</label>
+              </div>
+              <div>
+                <input type="text" className="contact-us-input-area" required />
+              </div>
+              <div className="contact-us-input-label">
+                <label>Phone number</label>
+              </div>
+              <div>
+                <input
+                  type="number"
+                  className="contact-us-input-area"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="contact-us-message">
+          <div className="contact-us-input-label">
+            <label>Message*</label>
+          </div>
+          <div>
+            <textarea
+              placeholder="Masukkan pesan anda"
+              className="contact-us-input-message"
+              required
+            />
+          </div>
+        </div>
+        <div>
+          <button
+            type="button"
+            className="contact-us-email-button"
+            onClick={() => alert("Still on development.")}
+          >
+            Email us
+          </button>
+        </div>
+      </div>
     </>
   );
 };
